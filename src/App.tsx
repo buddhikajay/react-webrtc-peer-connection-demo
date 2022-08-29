4dcb9b36096f4b63a2dda0ea4a8e455e2761d818
Buddhika Jayawardhana <buddhika.anushka@gmail.com>
2022-08-30 09:07:11 +1000

    Format App

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,100 @@
-import { useState } from 'react';
-import { CAPTURE_USER_MEDIA, CREATE_PEER_CONNECTIONS, START_NEGOTIATION, NEGOTIATING, NEGOTIATION_COMPLETED, STOP_STREAMING } from './constants'
-import { captureLocalStream, getLocalStream, stopMediaStream } from './Utils/MediaCaptureUtils';
-import { createPeerConnections, startStreaming, getRemoteStream, stopPeerConnections, getLocalPeerStats, getRemotePeerStats } from './Utils/WebRTCUtils';
-import VideoComponent from './Components/VideoComponent/VideoComponent';
-import ButtonComponent from './Components/ButtonComponent/ButtonComponent';
-import ConnectionStatsComponent from './Components/ConnectionStatsComponent/ConnectionStatsComponent';
+import { useState } from "react";
+import {
+  CAPTURE_USER_MEDIA,
+  CREATE_PEER_CONNECTIONS,
+  START_NEGOTIATION,
+  NEGOTIATING,
+  NEGOTIATION_COMPLETED,
+  STOP_STREAMING,
+} from "./constants";
+import {
+  captureLocalStream,
+  getLocalStream,
+  stopMediaStream,
+} from "./Utils/MediaCaptureUtils";
+import {
+  createPeerConnections,
+  startStreaming,
+  getRemoteStream,
+  stopPeerConnections,
+  getLocalPeerStats,
+  getRemotePeerStats,
+} from "./Utils/WebRTCUtils";
+import VideoComponent from "./Components/VideoComponent/VideoComponent";
+import ButtonComponent from "./Components/ButtonComponent/ButtonComponent";
+import ConnectionStatsComponent from "./Components/ConnectionStatsComponent/ConnectionStatsComponent";
 
 const App = () => {
-
   const [nextAction, setNextAction] = useState(CAPTURE_USER_MEDIA);
-  const [controlButtonText, setControlButtonText] = useState('Capture User Media');
+  const [controlButtonText, setControlButtonText] =
+    useState("Capture User Media");
   const initializeStreams = async () => {
-   await captureLocalStream();
-  }
+    await captureLocalStream();
+  };
 
-  const startVideoStreaming = async ()=> {
-    const localStream  = await getLocalStream();
+  const startVideoStreaming = async () => {
+    const localStream = await getLocalStream();
     await startStreaming(localStream);
-  }
+  };
 
-  const stopStreaming = async() => {
-    const localStream  = await getLocalStream();
+  const stopStreaming = async () => {
+    const localStream = await getLocalStream();
     await stopMediaStream(localStream);
     await stopPeerConnections();
-  }
+  };
 
-  const handleButtonClick  = async () => {
+  const handleButtonClick = async () => {
     console.log(nextAction);
-    switch(nextAction) {
+    switch (nextAction) {
       case CAPTURE_USER_MEDIA:
         await initializeStreams();
         setNextAction(CREATE_PEER_CONNECTIONS);
-        setControlButtonText('Create Peer Connections');
+        setControlButtonText("Create Peer Connections");
         break;
       case CREATE_PEER_CONNECTIONS:
         createPeerConnections();
         setNextAction(START_NEGOTIATION);
-        setControlButtonText('Start Negotiation');
+        setControlButtonText("Start Negotiation");
         break;
       case START_NEGOTIATION:
         setNextAction(NEGOTIATING);
         await startVideoStreaming();
         setNextAction(NEGOTIATION_COMPLETED);
         setNextAction(STOP_STREAMING);
-        setControlButtonText('Stop Streaming');
+        setControlButtonText("Stop Streaming");
         break;
       case STOP_STREAMING:
         setNextAction(CAPTURE_USER_MEDIA);
-        setControlButtonText('Capture User Media');
+        setControlButtonText("Capture User Media");
         stopStreaming();
         break;
       default:
-        return
+        return;
     }
-  }
+  };
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'spaceAround',
-      alignItems: 'center'
-    }}>
-      <section style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'spaceAround'
-        
-      }}>
-        <div style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "spaceAround",
+        alignItems: "center",
+      }}
+    >
+      <section
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "spaceAround",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
           <ConnectionStatsComponent
             getStats={getLocalPeerStats}
             ready={nextAction > NEGOTIATION_COMPLETED}
@@ -81,11 +104,13 @@ const App = () => {
             getMediaStream={getLocalStream}
           />
         </div>
-        <div style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
           <ConnectionStatsComponent
             getStats={getRemotePeerStats}
             ready={nextAction > NEGOTIATION_COMPLETED}
@@ -96,18 +121,17 @@ const App = () => {
           />
         </div>
       </section>
-      <section style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center'
-      }}>
-        <ButtonComponent
-          onClick={handleButtonClick}
-          text= {controlButtonText}
-        />
+      <section
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <ButtonComponent onClick={handleButtonClick} text={controlButtonText} />
       </section>
     </div>
   );
-}
+};
 
 export default App;
